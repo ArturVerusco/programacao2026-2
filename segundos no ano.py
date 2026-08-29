0b1f9921327da1ca6f6b993d6d0e41842e2d224f
Artur Veras <verasartur10@gmail.com>
2026-08-28 21:49:58 -0300

    adf

--- segundos no ano.py
+++ segundos no ano.py
@@ -4,4 +4,8 @@ dia = hora * 24
 semana = dia * 7
 ano = dia * 365
 
-print(ano)
+print("Segundos na semana: " + ano)
+print("Segundos na semana: " + semana)
+print("Segundos na dia: " + dia)
+print("Segundos na semana: " + hora)
+print("Segundos na semana: " + minuto)
