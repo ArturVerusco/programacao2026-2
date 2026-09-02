f201db5eed3142512d355ff3cacf1bf5397ddcbd
ArturVeras <verasartur10@gmail.com>
2026-09-02 17:35:47 +0000

    inteiro

--- calculadora_menu.py
+++ calculadora_menu.py
@@ -48,7 +48,7 @@ while a != 3:
 
     elif a == 2:
         
-        n1 = float(input("Digite um Ângulo: "))
+        n1 = int(input("Digite um Ângulo: "))
 
         if n1 < 90:
             print(n1, " é um Ângulo Agudo!")
